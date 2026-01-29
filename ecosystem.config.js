6956f581173c23882c7e1ff070f7fcae1e56afba
kkarki7120 <kkarki7120@gmail.com>
2026-01-29 10:17:42 +0545

    updated ecoconfig.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,15 @@ module.exports = {
   apps: [
     {
       name: "kushal-portfolio",
+      cwd: "/home/ubuntu/kushal_website",
       script: "npm",
       args: "start",
       interpreter: "none",
       env: {
-        NODE_ENV: "production"
-      }
+        NODE_ENV: "production",
+        PORT: 3000
+      },
+      post_update: "npm install && npx prisma generate && npm run build"
     }
   ]
 };
